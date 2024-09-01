effa4695bd9892ce1accd445037622c4cf4e7553
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-09-01 11:02:52 +0900

    JSON形式のデータを扱いやすいように整形

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,16 +1,10 @@
 <?php
-
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 class User extends Authenticatable
 {
-    use  Notifiable;
-
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
@@ -20,29 +14,29 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'uid',
+        'uid',  // 追加: uid カラムをフィル可能にする
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function todos()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Todo::class);
     }
 }
